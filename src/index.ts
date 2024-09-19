3ac29133e024151632cce254a5ce9bc735b92c03
Afshin T. Darian <git@darian.link>
2024-09-19 11:22:39 +0100

    add provider

--- src/index.ts
+++ src/index.ts
@@ -2,49 +2,74 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { Token } from '@lumino/coreutils';
 
 const COMMAND_EVENT_VERSION = '1';
 const COMMAND_EVENT = `https://quantstack.net/schema/metrics/command/v${COMMAND_EVENT_VERSION}`;
 
-/**
- * Initialization data for the @quantstack/metrics extension.
- */
+export const IMetricsProvider = new Token('@quantstack/metrics:provider');
+
+export interface IMetricsProvider {
+  collect: (event: any) => Promise<void>;
+}
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: '@quantstack/metrics:plugin',
   description: 'An extension for metrics',
   autoStart: true,
+  requires: [IMetricsProvider],
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension @quantstack/metrics is activated!');
-
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('@quantstack/metrics settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for @quantstack/metrics.', reason);
-        });
-    }
-    app.commands.commandExecuted.connect((_, command) => {
-      const { args, id } = command;
-      app.serviceManager.events.emit({
-        schema_id: COMMAND_EVENT,
-        data: { metrics: { command: id, args: args as unknown as any } },
-        version: COMMAND_EVENT_VERSION,
-      });
-    });
-    void (async () => {
-      for await (const event of app.serviceManager.events.stream) {
-        if (event.schema_id === COMMAND_EVENT) {
-          console.log('command event:', event);
-        }
+  activate: async (
+    app: JupyterFrontEnd,
+    provider: IMetricsProvider,
+    registry: ISettingRegistry | null
+  ) => {
+    try {
+      if (registry) {
+        const settings = await registry.load(plugin.id);
+        console.log(`${plugin.id} settings loaded:`, settings.composite);
       }
-    })();
+    } catch (error) {
+      console.error(`Failed to load ${plugin.id} settings.`, error);
+    }
+    void Private.broadcastEmissions(app);
+    void Private.handleEmissions(app, provider);
   }
 };
 
-export default plugin;
+const provider: JupyterFrontEndPlugin<IMetricsProvider> = {
+  id: '@quantstack/metrics:provider',
+  description: 'A provider for metrics',
+  provides: IMetricsProvider,
+  activate: () => ({ collect: async () => undefined })
+};
+
+export default [plugin, provider];
+
+namespace Private {
+  export async function broadcastEmissions({
+    commands,
+    serviceManager: { events }
+  }: JupyterFrontEnd) {
+    commands.commandExecuted.connect((_, { args, id }) => {
+      events.emit({
+        schema_id: COMMAND_EVENT,
+        data: { metrics: { command: id, args: args as unknown as any } },
+        version: COMMAND_EVENT_VERSION
+      });
+    });
+  }
+
+  export async function handleEmissions(
+    { serviceManager: { events } }: JupyterFrontEnd,
+    provider: IMetricsProvider
+  ) {
+    for await (const event of events.stream) {
+      if (event.schema_id === COMMAND_EVENT) {
+        console.log('emission!', event);
+        void provider.collect(event);
+      }
+    }
+  }
+}
